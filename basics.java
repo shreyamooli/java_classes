9380212fd35fdcc451c4c23e386c30b0aab3d2cf
Shreya Mooli <73119476+shreyamooli@users.noreply.github.com>
2023-02-20 10:40:35 +0530

    Create basics.java

---
+++ basics.java
@@ -0,0 +1,23 @@
+
+package javainonego;
+
+import java.util.Scanner;
+
+public class firstjavaclass {
+
+	public static void main(String[] args) 
+	
+	{
+		System.out.println("heyyy shrey");
+		int x=10,y=20;
+		double z= (double)x / (double) y;
+		System.out.println(z+x);
+		
+		Scanner sc=new Scanner (System.in);
+		int age=sc.nextInt();
+		
+	System.out .println(age+z);
+	}
+  }
+  
+		
